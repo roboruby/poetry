042d970d7351711fe5b707bbc448c10b4b6a1281
Matt Solt <mattsolt@gmail.com>
2026-09-20 10:59:14 -0400

    Version 0.1.6

--- gems/poetry-extract/lib/poetry/extract/version.rb
+++ gems/poetry-extract/lib/poetry/extract/version.rb
@@ -3,6 +3,6 @@
 module Poetry
   module Extract
     # The gem version.
-    VERSION = "0.1.5"
+    VERSION = "0.1.6"
   end
 end
--- gems/poetry-ui/lib/poetry/ui/version.rb
+++ gems/poetry-ui/lib/poetry/ui/version.rb
@@ -3,6 +3,6 @@
 module Poetry
   module Ui
     # The gem version.
-    VERSION = "0.1.5"
+    VERSION = "0.1.6"
   end
 end
